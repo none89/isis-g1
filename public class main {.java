a45d26fb227f8e0b1d411b29412286878c9f3440
none89 <162585924+none89@users.noreply.github.com>
2024-04-22 16:21:16 -0300

    Add files via upload

---
+++ public class main {.java
@@ -0,0 +1,30 @@
+public class main {
+    public static void main(string[] args){
+        Stack<String> pilhaCarros = new Stack<>();
+pilhaCarros.push("chevet"); 
+pilhaCarros.push("impala");
+pilhaCarros.push("ferrari");
+carroremovido=pilhacarros.pop();
+carrotopo=pilhacarros.peek();
+pilhavazia=pilhacarros.isEmpty();
+system.out.print("elemento no topo da pilha:"+ carrotopo);
+system.out.print("elemento removido da pilha: " + carroremovido);
+system.out.print("a pilha está vazia?" + pilha vazia)
+  }
+} 
+public class main {
+    public static void main (string[] args){
+
+Queue<String> filaCarros = new LinkedList<>();
+filaCarros.add("branco"); 
+filaCarros.add("preto");
+filaCarros.add("vermelho");
+string primeirocarro=filacarros.peek();
+string carroremovido=filacarros.poll();
+boolean filavazia=filacarros.isEmpty();
+system.out.print("primeiro elemento da fila" + primeirocarro);
+system.out.print(" carro removido" + carroremovido);
+system.out,print(" a fila está vazia?" +filavazia);
+    
+    }
+}
